8a6dc2da37cf7b17d0c380e7fe791366e01ff253
Durgesh <durgesh@Durgeshs-MacBook-Air.local>
2025-08-31 19:28:39 +0530

    playing with infinet zoom in canvas

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,14 +1,18 @@
 import { WebSocket, WebSocketServer } from 'ws';
 import jwt, { JwtPayload } from "jsonwebtoken";
-import {JWT_SECRET} from "@repo/backend-common/config"
-import { prismaClient } from "@repo/db/client"
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { prismaClient } from "@repo/db/client";
+
 const wss = new WebSocketServer({ port: 8080 });
-interface User{
-  userId:string,
-  ws:WebSocket,
-  rooms:string[]
+
+interface User {
+  ws: WebSocket,
+  rooms: string[],
+  userId: string
 }
+
 const users: User[] = [];
+
 function checkUser(token: string): string | null {
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
@@ -27,38 +31,52 @@ function checkUser(token: string): string | null {
   }
   return null;
 }
-wss.on('connection', function connection(ws,request) {
+
+wss.on('connection', function connection(ws, request) {
   const url = request.url;
-  if(!url){
-    return
+  if (!url) {
+    return;
   }
-  const queryParams = new URLSearchParams(url.split("?")[1]);
-  const token = queryParams.get("token") ||""
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get('token') || "";
   const userId = checkUser(token);
-  if(userId == null){
+
+  if (userId == null) {
     ws.close()
-    return null
+    return null;
   }
+
   users.push({
     userId,
-    rooms:[],
+    rooms: [],
     ws
   })
 
   ws.on('message', async function message(data) {
-    const parsedData = JSON.parse(data as unknown as string); 
-    if(parsedData.type =="join_room"){
-      const user = users.find(x => x.ws == ws);
-      user?.rooms.push(String(parsedData.roomId))
+    let parsedData;
+    if (typeof data !== "string") {
+      parsedData = JSON.parse(data.toString());
+    } else {
+      parsedData = JSON.parse(data); // {type: "join-room", roomId: 1}
     }
-    if(parsedData.type == "leave_room"){
-       const user = users.find(x => x.ws == ws);
-       if(!user){
-        return
-       }
-       user.rooms = user?.rooms.filter(x =>  x !== String(parsedData.roomId));
+
+    if (parsedData.type === "join_room") {
+      const user = users.find(x => x.ws === ws);
+      user?.rooms.push(parsedData.roomId);
     }
-    if(parsedData.type == "chat"){
+
+    if (parsedData.type === "leave_room") {
+      const user = users.find(x => x.ws === ws);
+      if (!user) {
+        return;
+      }
+      user.rooms = user?.rooms.filter(x => x === parsedData.room);
+    }
+
+    console.log("message received")
+    console.log(parsedData);
+
+    if (parsedData.type === "chat") {
       const roomId = parsedData.roomId;
       const message = parsedData.message;
 
@@ -69,15 +87,18 @@ wss.on('connection', function connection(ws,request) {
           userId
         }
       });
-      users.forEach(user =>{
-        if (user.rooms.includes(String(roomId))){
+
+      users.forEach(user => {
+        if (user.rooms.includes(roomId)) {
           user.ws.send(JSON.stringify({
-            type:"chat",
-            message,
+            type: "chat",
+            message: message,
             roomId
           }))
         }
       })
     }
+
   });
+
 });
