c54ad618a687e4f4476ec57bf197a91fcb2b5099
Durgesh <durgesh@Durgeshs-MacBook-Air.local>
2025-09-05 20:46:58 +0530

    changed credentials

--- apps/draw-frontend/config.ts
+++ apps/draw-frontend/config.ts
@@ -1,5 +1,7 @@
-export const BACKEND_URL="http://localhost:3001";
-export const WS_URL="ws://localhost:8080";
+
+// in apps/web/src/config.ts
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080";
 export const defaultStyles = "w-full flex items-center justify-center gap-2 bg-indigo-600 text-white font-bold text-lg p-4 rounded-md hover:bg-indigo-500 transition-colors transform hover:-translate-y-0.5"
 export const defaultStyles2 = "w-full flex items-center justify-center gap-2 bg-slate-800 text-white font-bold text-lg p-4 rounded-md hover:bg-slate-700 transition-colors transform hover:-translate-y-0.5"
 export const styleInputJoin = "w-full text-lg p-3 border-2 border-slate-800 rounded-md placeholder:text-slate-400 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition"
