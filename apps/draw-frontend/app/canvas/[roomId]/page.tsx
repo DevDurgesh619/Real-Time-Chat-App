a2a2adc61a40c4f76fb00812b9133d799038d9c3
Durgesh <durgesh@Durgeshs-MacBook-Air.local>
2025-10-09 20:27:58 +0530

    f_e fixed

--- apps/draw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/draw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,7 +1,7 @@
 import { RoomCanvas } from "@/components/RoomCanvas";
 
-export default function CanvasPage({ params }: { params: { roomId: string } }) {
-  const { roomId } = params;
+export default async function CanvasPage({ params }: { params: Promise<{ roomId: string }> }) {
+  const { roomId } = await params;
   console.log(roomId);
   console.log(typeof roomId);
   return <RoomCanvas roomId={roomId} />;
